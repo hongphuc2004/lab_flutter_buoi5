ac0ca3f2a2b776ced464a7f8c7fdc9d0d22c3cb7
hongphuc <phucpph.10a9bb.1920@gmail.com>
2026-05-06 15:48:01 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/phanphuochongphuc_2224802010871_lab5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.phanphuochongphuc_2224802010871_lab5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+final url = 'http://10.116.188.153:3000/';
+final registration = url + "register";
+final login = url + 'login';
+final addtodo = url + 'createToDo';
+final getToDoList = url + 'getUserTodoList';
+final deleteTodo = url + 'deleteTodo';
